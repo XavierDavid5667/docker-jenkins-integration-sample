6f88c03d19eb06534a466264fae3449a6d5cce2b
xavierdavid5667 <xavierdavid5667@gmail.com>
2023-06-06 12:04:11 +0530

    first commit

---
+++ src/main/java/com/example/dockerjenkinsIntegration/DockerjenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerjenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerjenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerjenkinsIntegration/DockerjenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerjenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
