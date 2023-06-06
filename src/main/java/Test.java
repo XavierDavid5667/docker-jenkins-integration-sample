92ba7b53589927b2670b18082f74e6ab38ff885b
xavierdavid5667 <xavierdavid5667@gmail.com>
2023-06-06 14:26:43 +0530

    added docker file

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hi how are you doing?");
+        System.out.println("Sample text");
     }
 }
